2749098b7004e02884a7141fe04c6120409e6bfd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-04 00:37:03 +0100

    [day 07] Implement eval function, needs reodering

--- src/07.hs
+++ src/07.hs
@@ -1,8 +1,11 @@
+import Data.Bits
 import Data.Char
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 import Text.ParserCombinators.ReadP
 
 
-partOne = id
+partOne = foldl eval Map.empty
 
 data Expr = AssignInt Int String
           | Assign String String
@@ -13,6 +16,25 @@ data Expr = AssignInt Int String
           | RShift String Int String
           | Not String String
           deriving (Show)
+
+eval :: Map String Int -> Expr -> Map String Int
+eval m (AssignInt n dst) = Map.insert dst n m
+eval m (Assign src dst) = Map.insert dst value m
+  where value = m!src
+eval m (AndInt n src dst) = Map.insert dst (n .&. value) m
+  where value = m!src
+eval m (And s1 s2 dst) = Map.insert dst (v1 .&. v2) m
+  where v1 = m!s1
+        v2 = m!s2
+eval m (Or s1 s2 dst) = Map.insert dst (v1 .|. v2) m
+  where v1 = m!s1
+        v2 = m!s2
+eval m (LShift src n dst) = Map.insert dst ((2^16-1) .&. (shiftL value n)) m
+  where value = m!src
+eval m (RShift src n dst) = Map.insert dst (shiftR value n) m
+  where value = m!src
+eval m (Not src dst) = Map.insert dst ((2^16-1) .&. (complement value)) m
+  where value = m!src
 
 ---
 -- Parsing
@@ -40,4 +62,4 @@ symbol = munch1 isAlpha
 ---
 -- Running
 
-main = interact $ unlines . map show . partOne . parse
+main = interact $ show . partOne . parse

24023b1f8eb85ce368097ace51087a78adf88552
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-03 19:26:06 +0100

    [day 04] Solve part two

--- src/04.py
+++ src/04.py
@@ -12,4 +12,13 @@ def part_one(code):
     return n
 
 
+def part_two(code):
+    n = 0
+    while not hashlib.md5((code+str(n)).encode('utf-8')).hexdigest().startswith('000000'):
+        n += 1
+        if (n % 1000000 == 0):
+            print(n)
+    return n
+
 print(part_one(code))
+print(part_two(code))

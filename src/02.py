be1c07465767fc5f7882017eafaf78ff2184f04b
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-03 18:41:14 +0100

    [day 02] Solve part two

--- src/02.py
+++ src/02.py
@@ -6,6 +6,14 @@ def area(xs):
     return min(l*w,w*h,h*l)+2*(l*w+w*h+h*l)
 
 
+def ribbon(xs):
+    l, w, h = xs
+    m = max(l,w,h)
+    return 2*(l+w+h-m) + l*w*h
+
+
 lines = [line.strip().split('x') for line in fileinput.input()]
 ns = [[int(x) for x in l] for l in lines]
+
 print(sum([area(x) for x in ns]))
+print(sum([ribbon(x) for x in ns]))

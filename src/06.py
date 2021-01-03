31a27fec1fa7e2d5a9f14900c4fbb0a9388b986d
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-03 21:45:56 +0100

    [day 06] Solve part two

--- src/06.py
+++ src/06.py
@@ -8,8 +8,16 @@ def part_one(lines):
     grid = [0]*1000000
     commands = [parse_line(line) for line in lines]
     for command in commands:
-        execute_command(grid, command)
+        execute_command_part_one(grid, command)
     return len(list(filter(lambda x: x == 1, grid)))
+
+
+def part_two(lines):
+    grid = [0]*1000000
+    commands = [parse_line(line) for line in lines]
+    for command in commands:
+        execute_command_part_two(grid, command)
+    return sum(grid)
 
 
 def parse_line(line):
@@ -17,7 +25,7 @@ def parse_line(line):
     return (result.group(1), (int(result[2]), int(result[3])), (int(result[4]), int(result[5])))
 
 
-def execute_command(grid, command):
+def execute_command_part_one(grid, command):
     c, (x1, y1), (x2, y2) = command
     for y in range(y1, y2+1):
         for x in range(x1, x2+1):
@@ -31,6 +39,20 @@ def execute_command(grid, command):
     return grid
 
 
+def execute_command_part_two(grid, command):
+    c, (x1, y1), (x2, y2) = command
+    for y in range(y1, y2+1):
+        for x in range(x1, x2+1):
+            offset = y*1000 + x
+            if (c == 'turn off'):
+                grid[offset] = max(0, grid[offset] - 1)
+            elif (c == 'turn on'):
+                grid[offset] = grid[offset] + 1
+            else:
+                grid[offset] = grid[offset] + 2
+    return grid
+
+
 if __name__ == "__main__" and not sys.flags.interactive:
     lines = [line.strip() for line in fileinput.input()]
-    print(part_one(lines))
+    print(part_two(lines))

19949b11fb21cd3e6dd81dc17ddbf50d65abfd3e
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-03 19:08:58 +0100

    [day 03] Solve part two

--- src/03.py
+++ src/03.py
@@ -6,15 +6,30 @@ import fileinput
 
 D = {'^': (0,1), '>': (1,0), 'v': (0,-1), '<': (-1,0)}
 
-(x, y) = (0,0)
-visits = {(x, y): 1}
-for c in commands:
-    (dx, dy) = D[c]
-    (x, y) = (x+dx, y+dy)
-    if (x, y) in visits:
-        visits[(x, y)] += 1
-    else:
-        visits[(x, y)] = 1
+
+def part_one(commands):
+    return len(visits(commands))
 
 
-print(len(visits))
+def part_two(commands):
+    santa = visits(commands[0::2])
+    robo_santa = visits(commands[1::2])
+    merged = dict()
+    merged.update(santa)
+    merged.update(robo_santa)
+    return len(merged)
+
+def visits(commands):
+    (x, y) = (0, 0)
+    visited = {(x, y): 1}
+    for c in commands:
+        (dx, dy) = D[c]
+        (x, y) = (x+dx, y+dy)
+        if (x, y) in visited:
+            visited[(x, y)] += 1
+        else:
+            visited[(x, y)] = 1
+    return visited
+
+print(part_one(commands))
+print(part_two(commands))

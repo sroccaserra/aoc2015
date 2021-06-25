c931334e729db053162222297755bb0f644780b1
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-06-25 22:36:21 +0200

    [day 20] Solve part two

--- src/20.py
+++ src/20.py
@@ -11,6 +11,28 @@ def solve_1(nb_presents):
     return n
 
 
+def solve_2(n):
+    limit = n//11+1
+    house = [0]*(limit)
+
+    print('building...')
+    for i in range(1, limit):
+        remaining = 50
+        for j in range(i, limit, i):
+            house[j] += i * 11
+            remaining -= 1
+            if 0 == remaining:
+                break
+
+    for i in range(1, 11):
+        print(house[i])
+
+    print('searching...')
+    for k in range(1, limit):
+        if house[k] >= n:
+            return k
+
+
 def presents(n):
     primes = prime_factors(n)
     c = Counter(primes)
@@ -38,3 +60,4 @@ if __name__ == "__main__" and not sys.flags.interactive:
     for n in range(1, 11):
         print(presents(n))
     print(solve_1(29000000))
+    print(solve_2(29000000))
